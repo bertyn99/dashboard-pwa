07c5d2d78e0786ba1c98a4d781bc8e78fe05dc59
bertyn99 <bboulikou@gmail.com>
2022-10-21 13:28:51 +0200

    ajout side bar et nav

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
+import Sidebar from "./components/Sidebar.vue";
+import Navbar from "./components/Navbar.vue";
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  
-  <HelloWorld msg="Vite + Vue" />
+  <div>
+    <Navbar />
+    <Sidebar />
+  </div>
 </template>
 
-<style scoped>
+<style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,3 @@
+<template>
+  <nav class="h-16 bg-gray-100 w-full shadow"></nav>
+</template>
